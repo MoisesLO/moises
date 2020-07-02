7f50f72d404122238f9cde11d1cf86a7e6b94d94
moises <elnaufrago2009@gmail.com>
2020-07-02 00:19:37 -0500

    navbar edit

---
+++ lib/navbar/navbar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints){
+        if(constraints.maxWidth>1200){
+          return DesktopNavBar();
+        }else if(constraints.maxWidth> 800 && constraints.maxWidth<1200){
+          return DesktopNavBar();
+        } else {
+          return MobileNavBar();
+        }
+      }
+    );
+  }
+}
+
+class DesktopNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _logoJPD(),
+          _menuNavbar()
+        ],
+      ),
+    );
+  }
+}
+
+Widget _logoJPD(){
+  return Container(
+    child: Row(
+      children: [
+        Image.asset('logo.jpg', width: 80, height: 80,),
+        Text('Subtitulo', style: TextStyle(color: Colors.white, fontSize: 32),)
+      ],
+    ),
+  );
+}
+
+Widget _menuNavbar(){
+  return Container(
+    child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text('Nosotros', style: TextStyle(color: Colors.white)),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text('Portafolio', style: TextStyle(color: Colors.white)),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text('Contactanos', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    ),
+  );
+}
+
+class MobileNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
